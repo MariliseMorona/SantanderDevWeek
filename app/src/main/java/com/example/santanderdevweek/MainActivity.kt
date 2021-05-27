17b9519addd32cbccdb4b93633d64a35d237dcdb
MariliseMorona <marymorona@gmail.com>
2021-05-27 09:54:41 -0300

    Termino do layout

--- app/src/main/java/com/example/santanderdevweek/MainActivity.kt
+++ app/src/main/java/com/example/santanderdevweek/MainActivity.kt
@@ -2,51 +2,31 @@ package com.example.santanderdevweek
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    //DEC VARIAVEIS
-        //val (value,not mutable)
-        //var (variable, it's mutable)
+        setSupportActionBar(findViewById(R.id.toolbar))
+    }
 
-        var contador : Int = 10
-        contador = 11
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflate = menuInflater
+        inflate.inflate(R.menu.main_menu, menu)
+        return true
+    }
 
-        val contador2 : Int = 11
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.item_1 -> {
+                Log.d("CLICK", "Click no item 1")
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
 
-        //null security, insert '?' for not ocurrent the NullPointerException
-        // with '?' do you talk the App, of the val can received the value null
-
-        val languageName: String? = null
-
-        //Conditiones
-        var idade = 20
-
-        if (idade > 18){
-            println("Você é maior de idade")
-        }else{
-            println("Você é menor de idade")
         }
-
-        val minhaIdade : String = if(idade > 18){
-            "Você pe maior de idade"
-        }else{
-            "Você é menor de idade"
-        }
-
-        //when
-        val suaIdade = when{
-            idade > 18 -> "Você é maior de idade"
-            else -> "Você é menor de idade"
-        }
-
-        fun qualIdade(): String{
-            return "Minha idade é $idade"
-        }
-
-        fun digaIdade() : String = "Minha idade é $idade"
-
-        val medicamento = Medicamento("Teste 1", "2x ao dia")    }
+    }
 }
