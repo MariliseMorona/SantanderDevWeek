ada2cf76c7fcd8160d4198748b1a691899a569f5
MariliseMorona <marymorona@gmail.com>
2021-05-27 11:14:55 -0300

    Responsividade

---
+++ app/src/main/java/com/example/santanderdevweek/data/local/FakeData.kt
@@ -0,0 +1,14 @@
+package com.example.santanderdevweek.data.local
+
+import com.example.santanderdevweek.data.Cartao
+import com.example.santanderdevweek.data.Cliente
+import com.example.santanderdevweek.data.Conta
+
+class FakeData {
+    fun getLocalData():Conta {
+        val cliente = Cliente ("Marilise")
+        val cartao = Cartao ("222222222")
+        val conta = Conta("020202-2", "0202-2", "1890,90", "4500,00", cliente, cartao)
+
+    }
+}
